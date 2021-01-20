894456b92b07a38053def8a27245ef9d8af74f46
andrew-lui <andrewrlui@hotmail.com>
2021-01-20 20:52:19 +1100

    Update perspective_transform.py

--- image-processing/perspective transform/perspective_transform.py
+++ image-processing/perspective transform/perspective_transform.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('..\images\sudoku.jpg')                                     # read image
+img = cv2.imread('..\images\sudoku.jpg')                              # read image
 
 origpts = np.float32([[56,65],[368,52],[28,387],[389,390]])           # four points on the original image - manually obtained corners
 newpts = np.float32([[0,0],[300,0],[0,300],[300,300]])                # four points on the new image - a desired 300x300 image
@@ -16,7 +16,7 @@ print(H)
 
 dst = cv2.warpPerspective(img, H, (300,300))                          # perform the perspective transform
 
-fig, (ax1, ax2) = plt.subplots(figsize = (18, 10), ncols = 2)         # show
+fig, (ax1, ax2) = plt.subplots(figsize = (15, 10), ncols = 2)         # show
 ax1.imshow(img), ax1.set_title("Original")
 ax2.imshow(dst), ax2.set_title("Transformed")
 plt.show() 
